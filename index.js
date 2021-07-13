995c9c1a22f167748049c0df76e548c3935d0dd9
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-07-13 12:39:04 -0700

    task 7 eradicated

--- index.js
+++ index.js
@@ -266,11 +266,25 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(num){
+  if (num < 100 && num >= 90){
+    return 'you got an A';
+  }
+  else if (num <= 89 && num >= 80){
+    return 'you got a B';
+  }
+  else if (num <= 79 && num >= 70){
+    return 'you got a C';
+  }
+  else if (num <= 69 && num >= 60){
+    return 'you got a D';
+  }
+  else {
+    return 'you got an F';
+  }
 }
 
-
+console.log('task7', grade(82));
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
 
