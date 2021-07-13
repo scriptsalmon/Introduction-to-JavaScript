ca838c68c3f1f8e7c48c60444bbead48d355bf89
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-07-13 12:24:17 -0700

    task 5 complete

--- index.js
+++ index.js
@@ -212,11 +212,11 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(km){
+  return km * 0.621371;
 }
 
-
+console.log('task5', miles(5));
 
 //Task 5b - Feet to CM
 /*
